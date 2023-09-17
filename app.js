923c6cab7f14bbd3caab438d7c03f2a86df8fa6a
NEBULA1978 <rpascualey@gmail.com>
2023-09-17 12:38:10 +0200

    Subo selector arriba de la primera foto añado vue3 y cambio a reactivo separo javasipt en app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      selectedCategory: 'todos',
+      selectedPrice: 'todos',
+      camisetas: [
+        { nombre: "Camiseta Manga Corta 1", categoria: "manga-corta", precio: 15.99, imagen: "./img/photo_1.jpg" },
+        { nombre: "Camiseta Manga Corta 2", categoria: "manga-corta", precio: 22.99, imagen: "./img/photo_10.jpg" },
+        { nombre: "Camiseta Manga Larga 1", categoria: "manga-larga", precio: 29.99, imagen: "./img/photo_11.jpg" },
+        { nombre: "Camiseta Manga Larga 2", categoria: "manga-larga", precio: 45.99, imagen: "./img/" },
+      ],
+    };
+  },
+  computed: {
+    filteredCamisetas() {
+      return this.camisetas.filter(camiseta => {
+        if (this.selectedCategory === 'todos' || camiseta.categoria === this.selectedCategory) {
+          if (this.selectedPrice === 'todos') {
+            return true;
+          } else if (this.selectedPrice === 'menos-de-20' && camiseta.precio < 20) {
+            return true;
+          } else if (this.selectedPrice === '20-50' && camiseta.precio >= 20 && camiseta.precio <= 50) {
+            return true;
+          } else if (this.selectedPrice === '50-o-mas' && camiseta.precio > 50) {
+            return true;
+          }
+        }
+        return false;
+      });
+    },
+  },
+});
+
+app.mount('#app');
